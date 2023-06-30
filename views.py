a5f423b27306946a902b42cf1f68215ed656042e
LeanLentil <matthew.biddick@icloud.com>
2023-07-01 00:23:58 +1200

    contact us form

--- views.py
+++ views.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from forms import ContactForm
+import pandas as pd
 
 views = Blueprint(__name__, "views")
 
 
-@views.route("/")
+@views.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("index.html")
+    form = ContactForm()
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        res = pd.DataFrame(
+            {"name": name, "email": email, "subject": subject, "message": message},
+            index=[0],
+        )
+        res.to_csv("./contactusMessage.csv")
+        print("The data are saved !")
+    else:
+        return render_template("index.html", form=form)
+
+    return render_template("index.html", form=form)
